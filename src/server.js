c4157532bf7a57be9460f91b1ba366e7cc014387
ahmadhamzh <ahmadhamzh@ymail.com>
2021-11-28 22:17:56 +0200

    add the lat & lon

--- src/server.js
+++ src/server.js
@@ -36,18 +36,20 @@ async function g() {
   for (let i = 0; i < usersinfo.length; i++) {
     msgQueue.companies[usersinfo[i].username] = { req: {} };
   }
-  console.log(msgQueue, "ooooooooooooooooooooooooooo");
 }
+
 g();
 setInterval(async function () {
   g();
 }, 600000);
+
 customs.on("connection", (socket) => {
   console.log("customs connected", socket.id);
   socket.on("rental-res", (payload) => {
     customs.emit("res", payload);
   });
 });
+
 owners.on("connection", (socket) => {
   console.log("owner connected", socket.id);
   socket.on("get-all", (payload) => {
@@ -55,33 +57,26 @@ owners.on("connection", (socket) => {
       owners.emit("all", id);
     });
   });
+
+  socket.on('getLocation', (payload) => {
+    socket.emit('getLocation', payload)
+  })
+
   socket.on("req-fromCus", async (payload) => {
     let id = payload.carid;
     let carrecored = await car.findOne({ where: { id } });
     let userinfo = await user.findOne({ where: { id: carrecored.ownerId } });
-    console.log(userinfo.username);
-    // console.log();
-    // for (let i = 0; i < msgQueue.cars.length; i++) {
-    //   if (msgQueue.cars[i][0] == payload.carid) {
-    //     msgQueue.companies[msgQueue.cars[i][1]].req[
-    //       payload.carid
-    //     ] = `there is a customer need a car that has id:${payload.carid} from ${payload.startDate} to ${payload.endDate} `;
-
-    //     break;
-    //   }
-    // }
     payload.ownerName = userinfo.username;
-    payload.carName=carrecored.name
+    payload.carName = carrecored.name
     msgQueue.companies[userinfo.username].req[
       payload.carid
     ] = `there is a customer need a car that has id:${payload.carid} has name${payload.carName} from ${payload.startDate} to ${payload.endDate} `;
-console.log(msgQueue.companies[userinfo.username].req);
+    console.log(msgQueue.companies[userinfo.username].req);
     owners.emit("rent-req", payload);
   });
 });
 
-drivers.on("connection", (socket) => {
-  // console.log("driver connected");
+drivers.on("connection", (socket) => {  
   socket.on("custom-need-driver", (payload) => {
     drivers.emit("mission", payload);
   });
--- src/socket/module/customer/custom.js
+++ src/socket/module/customer/custom.js
@@ -1,16 +1,16 @@
 "use strict";
-// const faker = require("faker");
+
 const io = require("socket.io-client");
 const host = "http://localhost:3000";
 const ownersConnection = io.connect(`${host}/owners`);
 const customConnection = io.connect(`${host}/customs`);
+
 let pickup = {
   carid: `${process.argv[2]}`,
   startDate: `${process.argv[3]}`,
   endDate: `${process.argv[4]}`,
 };
-console.log(process.argv[0],"00000");
-console.log(process.argv[1],"q1111");
+
 if (pickup.carid === "undefined") {
   console.log("please enter a car id");
 } else if (pickup.startDate === "undefined") {
